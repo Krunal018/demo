28b3f8743efe9b5ffa4cc0cf01899a07f7b0002d
Krunal018 <krunalmangukiya18@gmail.com>
2024-09-12 17:03:29 +0530

    add 18

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">krunal mangukiya</div>;
+  return <div className="App">krunal mangukiya 18</div>;
 }
 
 export default App;
